a416e7b66dbe8515ba618cc3cfeccc1f6189fe45
AnaRativa09 <anarativa09@gmail.com>
2020-11-23 15:24:40 -0500

    Create react app

---
+++ src/controller/main.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAnJZqnL-c13J6UG8VhIcRREikTgfo3r2c",
+    authDomain: "nexa-lh.firebaseapp.com",
+    databaseURL: "https://nexa-lh.firebaseio.com",
+    projectId: "nexa-lh",
+    storageBucket: "nexa-lh.appspot.com",
+    messagingSenderId: "492935503772",
+    appId: "1:492935503772:web:62df276f3bc2fc3833b259"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
