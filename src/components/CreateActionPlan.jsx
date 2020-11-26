34bd91214d01c0c9b772144eb24ea33da936201f
AnaRativa09 <anarativa09@gmail.com>
2020-11-26 03:14:54 -0500

    Change select

--- src/components/CreateActionPlan.jsx
+++ src/components/CreateActionPlan.jsx
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Button, Form } from "react-bootstrap";
+import React from 'react';
+import { Form } from "react-bootstrap";
 
 const CreateActionPlan = ({infoSupervision}) => {
 
@@ -7,16 +7,18 @@ const CreateActionPlan = ({infoSupervision}) => {
     return (
         <>
             <div className="info-supervision">
-                <h3>{infoSupervision.unidad}</h3>
-                <p>Fecha de Inicio: {infoSupervision.startDate}</p>
-                <p>Fecha de Cierre: {infoSupervision.expirationDate}</p>
-                {/* <Form.Group controlId="exampleForm.ControlSelect1">
-                    <Form.Control as="select" name="findings" value={} required >
-                        <option value="selecciona">Selecciona Unidad</option>
-                        <option value="Si">Si</option>
-                        <option value="No">No</option>
-                    </Form.Control>
-                </Form.Group> */}
+                <Form className="form-container">
+                    <h3>{infoSupervision.unidad}</h3>
+                    <p>Fecha de Inicio: {infoSupervision.startDate}</p>
+                    <p>Fecha de Cierre: {infoSupervision.expirationDate}</p>
+                    <Form.Group controlId="exampleForm.ControlSelect1">
+                        <Form.Control as="select" name="findings" value="" required >
+                            <option value="selecciona">Se presentaron hallazgos</option>
+                            <option value="Si">Si</option>
+                            <option value="No">No</option>
+                        </Form.Control>
+                    </Form.Group>
+                </Form>
             </div>
         </>
     );
