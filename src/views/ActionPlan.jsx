067b9033d4f2418848ec43422da9ab31d3b82944
roxanaguerra <sguerraapaza@gmail.com>
2020-11-26 01:21:23 -0500

    update file, borrando lineas no necesarias

--- src/views/ActionPlan.jsx
+++ src/views/ActionPlan.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
 import Header from '../components/Header';
-import UploadImage from '../components/UploadImage';
 
 const ActionPlan = () => {
     return(
         <>
             <Header name="Plan de acción" />
-            <UploadImage />
             <NavBar />
         </>
     )
