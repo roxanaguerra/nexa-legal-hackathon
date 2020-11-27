02d0c931565dd04c65c8d2d14b171970001d407e
AnaRativa09 <anarativa09@gmail.com>
2020-11-27 12:18:15 -0500

    Styles profile

--- src/views/UserProfile.jsx
+++ src/views/UserProfile.jsx
@@ -7,44 +7,53 @@ import userPhoto from '../assets/images/user-photo.jpg'
 import logoNexaSuma from '../assets/images/logo-nexa-suma.png'
 import Header from '../components/Header';
 
- const UserProfile = () => {
+const UserProfile = () => {
 
     const history = useHistory();
 
     // Cerrar Sesión
-const signOut = () => firebase.auth().signOut()
-.then(() => {
-  localStorage.removeItem('username');
-//   localStorage.removeItem('profileImg');
-console.log('Saliendo...!');
-history.push('/');
-
-})
-.catch(() => {
-// console.log(error);
-});
+    const signOut = () => firebase.auth().signOut()
+    .then(() => {
+        localStorage.removeItem('username');
+        console.log('Saliendo...!');
+        history.push('/');
+    })
+    .catch(() => {
+    // console.log(error);
+    });
 
     return (
         <div className="container-userProfile">
-            <Header name ="Perfil"/>
+            <Header name ="PERFIL"/>
+
             <button  className="btn-back">
                 <i className="fas fa-arrow-left btn-back-i" ></i>
             </button>
+
             <div className ="userPhoto-userProfile">
-              <img src={userPhoto}alt=""/>
+                <img src={userPhoto}alt=""/>
             </div>  
-            <div className="nameUser-userProfile"><p>DIANA MADRID GÓMEZ</p></div>
-            <div className="leader-userProfile"><p>líder Unidad de Medio Ambiente</p></div>
-            <div className="emailUser-userProfile"><p>nexa.ambiente.20@gmail.com</p></div>
+
+            <div className="nameUser-userProfile">
+                <p>DIANA MADRID GÓMEZ</p>
+            </div>
+            
+            <>
+                <p className="orange-text">Líder Unidad de Medio Ambiente</p>
+                <p>nexa.ambiente.20@gmail.com</p>
+            </>
+
+            <div className="emailUser-userProfile"></div>
 
             <div className="logo-userProfile">
                 <img src={logoNexaSuma}alt=""/>
             </div>
-           
+
             <div className="links-userProfile">
                 <p>Perfil de Usuario</p>
                 <p>Notificaciones</p>
             </div>
+
             <div className="close-userProfile">
                 <button onClick={signOut}>Cerrar Sesión</button>
             </div>
