fb900268da4e8dbda67cfd394b6ca431ea901c8c
roxanaguerra <sguerraapaza@gmail.com>
2020-11-27 12:38:28 -0500

    update de setTimeout de modal

--- src/components/ActualSupervision.jsx
+++ src/components/ActualSupervision.jsx
@@ -56,7 +56,7 @@ const ActualSupervision = ({infoSupervision}) => {
         setTimeout(() => {     
             setConfirmationSendFinal(false);
             history.push('/accion');
-        }, 2000);
+        }, 4000);
     }
 
     return (
